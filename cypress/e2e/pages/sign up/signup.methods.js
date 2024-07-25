8ca78ce3969ee44b23eabeb56c8cf53617a53354
Cristal Accinelli <accinelli3@gmail.com>
2024-07-25 20:28:33 -0300

    sign up page creation

---
+++ cypress/e2e/pages/sign up/signup.methods.js
@@ -0,0 +1,24 @@
+import { SignupElements } from './pages/sign up/signup.elements';
+
+export class SignupMethods{
+    static insertUsername(username) {
+        SignupElements.textboxes.username.invoke('val', username);
+    }
+
+    static insertPassword(password) {
+        SignupElements.textboxes.password.invoke('val', password);
+    }
+
+
+    static clickOnSignupButton() {
+       SignupElements.buttons.signup.click();
+    }
+
+    static signup(username,password) {
+        this.insertUsername(username)
+        this.insertPassword(password)
+        this.clickOnSignupButton()
+    }
+}
+
+
