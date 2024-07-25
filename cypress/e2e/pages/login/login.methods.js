10ecd5471a61d9ca02b5f837f5f9a8562ef838c3
Cristal Accinelli <accinelli3@gmail.com>
2024-07-25 20:28:16 -0300

    login page creation

---
+++ cypress/e2e/pages/login/login.methods.js
@@ -0,0 +1,25 @@
+import { LoginElements } from './login.elements';
+
+
+export class LoginMethods {
+    static insertUsername(username) {
+        LoginElements.textboxes.username.invoke('val', username);
+    }
+
+    static insertPassword(password) {
+        LoginElements.textboxes.password.invoke('val', password);
+    }
+
+
+    static clickOnLoginButton() {
+       LoginElements.buttons.login.click();
+    }
+
+    static login(username,password){
+        this.insertUsername(username)
+        this.insertPassword(password)
+        this.clickOnLoginButton()
+    }
+
+}
+
