ee6dfa7d6d736624c4bdc64bb4eab9857f5979c6
Cristal Accinelli <accinelli3@gmail.com>
2024-07-25 20:28:56 -0300

    product details creation page

---
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,7 @@
+import { ProductDetailsElements } from "./product-details.elements";
+
+export class ProductDetailMethods {
+static clickOnAddToCartButton() {
+ProductDetailsElements.buttons.addToCart.click();
+}
+}
